a30f98b03105bfa9c1e72c48737d88b0860e6ca2
lipmtb <52589058+lipmtb@users.noreply.github.com>
2019-10-18 22:43:51 +0800

    Update CreateToken.java
    update token

--- wxMobileApp/src/com/wxapp/util/CreateToken.java
+++ wxMobileApp/src/com/wxapp/util/CreateToken.java
@@ -2,8 +2,10 @@ package com.wxapp.util;
 
 public class CreateToken {
    public static String createTokenStr(String openId,String sessionId) {
+	   
 	   String token=null;
 	   token=openId+sessionId;
 	   return token;
+	   
    }
 }
